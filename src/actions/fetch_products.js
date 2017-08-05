71bed4a7fa424c93155b17b2fe20923cefec9d83
Flosloot <SlootSantos@users.noreply.github.com>
2017-08-05 19:21:56 +0200

    comment proxy issue // rm debugger

--- src/actions/fetch_products.js
+++ src/actions/fetch_products.js
@@ -4,10 +4,8 @@ import { API_URL } from '../config/store_config';
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 const BASE_URL = API_URL;
 
-debugger
-
-
 // export fetch products function
+// right now working fine without base path bc of proxy // WON'T work in prod!!
 export function fetchProducts() {
   const route = `/products`;
   const request = axios.get(`${route}`);
