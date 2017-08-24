a0539169600481fefbbfc55e9900637262fe3607
Flo Sloot <florian.sloot@aboutyou.de>
2017-08-24 18:51:27 +0200

    fix err related to sdk

--- src/config/sdk_config.js
+++ src/config/sdk_config.js
@@ -1,4 +1,4 @@
 import { API_URL, STORE_ID } from './store_config';
-import { EcSdk } from 'ecommerce-js-sdk';
+import EcSdk from 'ecommerce-js-sdk';
 
 export const sdk = new EcSdk(API_URL, STORE_ID);
