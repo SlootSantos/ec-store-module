690f399ad5352c79fb9a4eb1d0a37ec37d655c56
Flo Sloot <florian.sloot@aboutyou.de>
2017-09-30 16:52:17 +0200

    [ENV_DEV] prepare soft launch

--- src/container/landing_page.js
+++ src/container/landing_page.js
@@ -20,7 +20,7 @@ class LandingPage extends Component {
         name: 'Cà Phe Phin',
         id: 'ed0ee098-d5ba-409d-b2f4-c9610feca4b4',
         imgSrc: 'https://www.huber-kaffee.de/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/m/a/marisma_shop_big.png',
-        description: 'Der traditionell vietnamesische Kaffee zeichnet sich durch seine sehr starke Röstung aus, die dem Kaffee einen sehr kräftigen Geschmack geben. Traditionell wird er mit gesüßter Kondensmilch auf Eis serviert, die die starke Röstung ausbalanciert und die Schokonoten der Bohnen hervorheben und den Kaffee cremig machen.',
+        description: 'Der <b>traditionell vietnamesische Kaffee</b> zeichnet sich durch seine sehr starke Röstung aus, die dem Kaffee einen sehr kräftigen Geschmack geben. Traditionell wird er mit gesüßter Kondensmilch auf Eis serviert, die die starke Röstung ausbalanciert und die Schokonoten der Bohnen hervorheben und den Kaffee cremig machen.',
         bullets: ['Kräftiges Aroma', 'Schokoladige Noten', '70% Arabica, 30 % Robusta'],
         price: '€8,90'
       },
@@ -28,7 +28,7 @@ class LandingPage extends Component {
         name: 'Thanh Trùc',
         id: 'ed0ee098-d5ba-409d-b2f4-c9610feca4b4',
         imgSrc: 'https://www.huber-kaffee.de/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/m/a/marisma_shop_big.png',
-        description: 'Unser Filterkaffee ist ein sortenreiner Kaffee aus 100% Bourbon Bohnen, eine sehr edle Sorte von Arabica Bohnen. Bourbon Bohnen sind bekannt dafür, dass sie eine sehr feine komplexe Säurestruktur hat, die den Kaffee sehr bekömmlich und fein machen.',
+        description: 'Unser <b>Filterkaffee</b> ist ein sortenreiner Kaffee aus 100% Bourbon Bohnen, eine sehr edle Sorte von Arabica Bohnen. Bourbon Bohnen sind bekannt dafür, dass sie eine sehr feine komplexe Säurestruktur hat, die den Kaffee sehr bekömmlich und fein machen.',
         bullets: ['Ausgewogenge, geringe Säure', 'Feiner und milder Kaffee', '100% Bourbon Arabica'],
         price: '€12,90'
       },
@@ -36,7 +36,7 @@ class LandingPage extends Component {
         name: 'Dình Huy',
         id: 'ed0ee098-d5ba-409d-b2f4-c9610feca4b4',
         imgSrc: 'https://www.huber-kaffee.de/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/m/a/marisma_shop_big.png',
-        description: 'Der Espressokaffee hat einen sehr hohen Arabica Anteil, was dem Kaffee fruchtige Aromen verleiht. Da der vietnamesische Kaffee meistens einen höheren Koffeeinanteil haben, ist der Kaffee sehr kräftig und hat eine wunderbare Crema.',
+        description: 'Der <b>Espressokaffee</b> hat einen sehr hohen Arabica Anteil, was dem Kaffee fruchtige Aromen verleiht. Da der vietnamesische Kaffee meistens einen höheren Koffeeinanteil haben, ist der Kaffee sehr kräftig und hat eine wunderbare Crema.',
         bullets: ['Fruchtige Säure', 'Crema', '90% Arabica, 10% Robusta'],
         price: '€10,90'
       }
