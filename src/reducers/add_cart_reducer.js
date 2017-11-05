79a0aee0e811df1b0e469606b3336d7eec1735d0
flosloot <flosloot@gmx.de>
2017-11-05 18:15:54 +0100

    [ENV_DEV] fix error at add_cart reducer

--- src/reducers/add_cart_reducer.js
+++ src/reducers/add_cart_reducer.js
@@ -2,12 +2,11 @@
 import { ADD_CART } from '../actions/add_cart';
 // export reducer
 // returns all the items
-export default function(state = [], action) {
-
+export default function (state = [], action) {
    switch (action.type) {
      case ADD_CART:
 
-      return action.payload.data.data;
+      return action.payload ? action.payload.data.data : null;
     default:
     return state;
    }
