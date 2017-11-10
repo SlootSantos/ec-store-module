9641be9e0d3b021248bfb6f453c2f9311b9d7a11
flosloot <flosloot@gmx.de>
2017-11-10 17:35:13 +0100

    [IMPLEMENT_SHOP] rm reducer.. shouldn't reduce

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 // import products reducer
 import ProductsAllReducer from './products_all_reducer';
 import ProductSingleReducer from './product_single_reducer';
-import CheckoutReducer from './checkout_reducer';
+// import CheckoutReducer from './checkout_reducer';
 import GetCartReducer from './get_cart_reducer';
 import AddCartReducer from './add_cart_reducer';
 
@@ -12,7 +12,7 @@ const rootReducer = combineReducers({
   products: ProductsAllReducer,
   product: ProductSingleReducer,
   cart: GetCartReducer,
-  checkout: CheckoutReducer,
+  // checkout: CheckoutReducer,
   addProd: AddCartReducer
 });
 
