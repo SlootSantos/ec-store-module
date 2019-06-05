418987d851ea7128e0ec9ddc3f1e46e03e45511d
Flosloot <SlootSantos@users.noreply.github.com>
2019-06-05 10:52:07 +0200

    Update store_config.js

--- src/config/store_config.js
+++ src/config/store_config.js
@@ -1,5 +1,5 @@
-const STORE_ID = 'f48bc379d134c584f236f527c39bc605';
-const PAYMILL_PUBLIC_KEY = 'bc3c63a4db341d8c5a8bfe08b0965482';
+const STORE_ID = '--';
+const PAYMILL_PUBLIC_KEY = '--';
 const API_URL = process.env.REACT_APP_API_URL || 'https://custom-commerce.de';
 
 export {
